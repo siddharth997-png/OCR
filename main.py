df4ca146edfde1d99f164e5a23f7a1c94fc4615b
Siddharth Kothari <siddharthkothari655@gmail.com>
2022-02-10 14:45:56 +0530

    added comparison between real and converted images

--- main.py
+++ main.py
@@ -45,9 +45,16 @@ def findDimensions(text, attr) :
 
 def findProductProperties(text) :
     properties = {}
-    properties['AWB'] = findAwb(text)
+    awb = findAwb(text)
+    if(awb == "Not Found") :
+        awb = findDimensions(text, "Barcode")
     height = findDimensions(text, "Height")
+    if(height == "Not Found") :
+        heigh = findDimensions(text, "H")
     length = findDimensions(text, "Length")
+    if(length == "Not Found") :
+        length = findDimensions(text, "L")
+    properties['AWB'] = awb
     properties["Height"] = height
     properties["Length"] = length
     return properties
@@ -56,30 +63,41 @@ def findProductProperties(text) :
 
 myconfig = r"--psm 6"
 
-images = ["Aramex2_Sample.jpg",
-        "Aramex_Sample.jpg","BD_Sample_Image.jpg",
+converted_images = ["op1.jpg",
+        "op2.jpg",
+        "op3.jpg",
+        "op4.jpg",
+        "op5.jpg",
+        "op6.jpg",
+        "op7.jpg",
+        "op8.jpg",
+        "op9.jpg",
+        "op10.jpg",
+]
+
+og_images = ["Aramex_Sample.jpg",
+        "Aramex2_Sample.jpg",
+        "BD_Sample_Image.jpg",
         "Delhivery_Reverse_Sample_Image.jpeg",
         "Delhivery_Sample_Image.jpeg",
-        "Delhivery_Surface_10_Kg_Sample_Image.jpeg",
+        # "Delhivery_Surface_10_Kg_Sample_Image.jpeg",
         "Delhivery_Surface_Sample_Image.jpeg",
         "Ecom_Express_Reverse_Sample_Image.jpeg",
-        "Ecom_Express_ROS_Reverse_Sample_Image.jpeg","Ecom_Express_Surface_2kg_Sample_Image.jpeg",
-        "Ecom_Express_Surface_Sample_Image.jpg",
-        "SFX2_Sample.jpg",
-        "SFX_Sample.jpg",
+        "Ecom_Express_ROS_Reverse_Sample_Image.jpeg",
+        "Ecom_Express_Surface_2kg_Sample_Image.jpeg",
         "XB.jpg"
 ]
 
-for image in images:
-    image_path = "all_images/" + image
-    text = pytesseract.image_to_string(PIL.Image.open(image_path),config=myconfig)
-    
-    print("\nFor image ",image)
-    if(image == "SFX2_Sample.jpg"):
-        print("\nText : \n",text,"\n")
-    details = findProductProperties(text)
-    print(details)
-    
+for i in range(0,10):
+    converted_image_path = "converted/" + converted_images[i]
+    real_image_path = "all_images/" + og_images[i] 
+    print("\nFor image ",og_images[i])
+    text = pytesseract.image_to_string(PIL.Image.open(converted_image_path),config=myconfig)
+    print("Details from Converted Image : ")
+    print(findProductProperties(text))
+    text = pytesseract.image_to_string(PIL.Image.open(real_image_path),config=myconfig)
+    print("Details from Real Image : ")
+    print(findProductProperties(text))
     
 
   
